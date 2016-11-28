866dae4e2ff933b5aaeb2c74bbd936cae47f93b1
windr00 <imrwz@icloud.com>
2016-11-28 16:44:58 +0800

    Initial check-in of module UnitySolutionLauncher

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+using System;
+using System.IO;
+using System.Diagnostics;
+namespace UnitySolutionLauncher
+{
+	static class MainClass
+	{
+
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
